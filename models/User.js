260d212ceb7e6fec0a5bc0444ef41845e44ce160
MD Arif <leoarifuddin@gmail.com>
2025-02-01 03:01:07 +0600

    added

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, required: true },
     status: { type: String, default: 'pending' }, // 'approved', 'pending'
 });
 
