3dc2652fa6286a5f8f79551c1ca2475709cebeef
MD Arif <leoarifuddin@gmail.com>
2025-02-01 14:14:43 +0600

    added

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // No encryption
     role: { type: String, required: true },
-    status: { type: String, default: 'pending' }, // 'approved', 'pending'
+    status: { type: String, default: 'approved' }, // 'approved', 'pending'
 });
 
 const User = mongoose.model('User', userSchema);
