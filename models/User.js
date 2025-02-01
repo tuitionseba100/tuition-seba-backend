ed3c2aa43295ca92d1a4684bf20f697cf73a5fdc
MD Arif <leoarifuddin@gmail.com>
2025-02-01 13:57:48 +0600

    asd

--- models/User.js
+++ models/User.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }, // No encryption
     role: { type: String, required: true },
     status: { type: String, default: 'pending' }, // 'approved', 'pending'
-});
-
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 const User = mongoose.model('User', userSchema);
