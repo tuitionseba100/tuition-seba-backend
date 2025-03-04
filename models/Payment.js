3e631fdc32e38216b3e74b91db64cbc1da2f545d
MD Arif <leoarifuddin@gmail.com>
2025-03-04 11:07:42 +0600

    pushed

--- models/Payment.js
+++ models/Payment.js
@@ -5,6 +5,7 @@ const paymentSchema = new mongoose.Schema({
     tuitionCode: { type: String, required: true },
     tuitionId: { type: String, required: true },
     paymentReceivedDate: { type: Date },
+    duePayDate: { type: Date },
     paymentType: { type: String },
     transactionId: { type: String },
     tutorName: { type: String },
