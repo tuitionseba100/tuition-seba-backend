3699c409d7a4fa7b5686029519834dad7dd91816
MD Arif <leoarifuddin@gmail.com>
2025-03-15 21:23:18 +0600

    pushed

--- models/Attendance.js
+++ models/Attendance.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const AttendanceSchema = new mongoose.Schema({
     userId: { type: String, required: true },
-    userName: { type: String, required: true },
+    userName: { type: String },
     role: { type: String, required: true },
     startTime: { type: Date, required: true },
     endTime: { type: Date, default: null },
