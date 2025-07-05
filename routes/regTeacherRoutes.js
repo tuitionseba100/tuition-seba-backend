e507c6ef47db229721c833d687f68aafe0720b28
MD Arif <leoarifuddin@gmail.com>
2025-07-05 15:11:40 +0600

    dsdsdsdf

--- routes/regTeacherRoutes.js
+++ routes/regTeacherRoutes.js
@@ -5,14 +5,16 @@ const router = express.Router();
 const moment = require('moment-timezone');
 const path = require('path');
 
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-    if (extname && mimetype) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only .jpg, .jpeg, .png files are allowed'));
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access Denied' });
+
+    try {
+        const verified = jwt.verify(token, 'mahedi1000abcdefgh100');
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid Token' });
     }
 };
 
