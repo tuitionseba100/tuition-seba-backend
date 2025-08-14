7011ef9b7c5fd09c9a108a65f300858a0062546e
MD Arif <leoarifuddin@gmail.com>
2025-08-14 16:05:37 +0600

    sasasks

--- routes/tuitionApplyRoutes.js
+++ routes/tuitionApplyRoutes.js
@@ -278,7 +278,7 @@ router.get('/appliedListByTuitionId', async (req, res) => {
     try {
         const appliedList = await TuitionApply.find(
             { tuitionId },
-            'premiumCode name phone institute department address appliedAt status isSpam isBest'
+            'premiumCode name phone institute department address appliedAt status isSpam isBest comment commentForTeacher'
         ).sort({ appliedAt: -1 });
 
         res.json(appliedList);
