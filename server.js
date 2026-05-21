384847cbeea6220d506354a90439a3f691bf0d83
arif.uddin <arif.uddin@eunoiaits.com>
2026-05-21 23:48:58 +0600

    updates

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
