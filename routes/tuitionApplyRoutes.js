6a5f81f609d51f7217b11e926168d0e655f45652
arif.uddin <arif.uddin@eunoiaits.com>
2026-07-19 15:49:48 +0600

    fetch tuition note in backend

--- routes/tuitionApplyRoutes.js
+++ routes/tuitionApplyRoutes.js
@@ -534,11 +534,14 @@ router.get('/getTuitionStatusesByPhone', async (req, res) => {
                 .sort({ _id: 1 })
                 .lean();
             
+            const tuition = await Tuition.findOne({ tuitionCode: apply.tuitionCode }, 'note').lean();
+            
             const serialNumber = allApplies.findIndex(a => a._id.toString() === apply._id.toString()) + 1;
             return {
                 ...apply,
                 serialNumber: serialNumber > 0 ? serialNumber : 1,
-                totalApplies: allApplies.length
+                totalApplies: allApplies.length,
+                note: tuition ? tuition.note : ''
             };
         }));
 
