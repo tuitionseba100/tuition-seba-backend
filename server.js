646aec8e1acdc22ced8b2275415c3d4647e3d139
arif.uddin <arif.uddin@eunoiaits.com>
2026-08-03 12:03:11 +0600

    updates

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to TuitionSeba API!');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
