c806ec0b4203fc67338577f64f518dc7c3a61897
arif.uddin <arif.uddin@eunoiaits.com>
2026-08-13 14:45:05 +0600

    uipds

--- routes/tuitionRoutes.js
+++ routes/tuitionRoutes.js
@@ -50,6 +50,11 @@ const getLeastAssignedUser = async (userList) => {
 
     return counts[0].user;
 };
+//test route
+router.get('/test', (req, res) => {
+    res.send('hi tuition');
+});
+
 //available tuition
 router.get('/available', async (req, res) => {
     try {
